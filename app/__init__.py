8ac99a2aaa08dec8387dbc500a707d9af182d57c
Jerry Trymander <jert47@yahoo.com>
2020-01-25 13:42:53 +0100

    Chap 3

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)  # reads in cofiguration in config.py
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -17,3 +18,16 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    # the method below is very crucial. When the user presses "Submit", a POST
+    #  request is sent. The method gatheres data, runs validators, and if 
+    # everythin is OK, returns True.
+    # It returns False on GET request.
+    if form.validate_on_submit(): 
+        flash('Login requested for user {}: remember_me()={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
